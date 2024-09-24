0c3bbe523a7a4f31a7ee91c8a422b13b7654da4d
jcwang587 <jiachengwang@umass.edu>
2024-09-24 20:15:16 +0000

    add v2 conv

--- cgcnn2/cgcnn_model.py
+++ cgcnn2/cgcnn_model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 
-class ConvLayer(nn.Module):
+class ConvLayer_v1(nn.Module):
     """
     Convolutional operation on graphs
     """
@@ -19,7 +19,84 @@ class ConvLayer(nn.Module):
         nbr_fea_len: int
           Number of bond features.
         """
-        super(ConvLayer, self).__init__()
+        super(ConvLayer_v1, self).__init__()
+        self.atom_fea_len = atom_fea_len
+        self.nbr_fea_len = nbr_fea_len
+        self.fc_full = nn.Linear(
+            2 * self.atom_fea_len + self.nbr_fea_len, 2 * self.atom_fea_len
+        )
+        self.sigmoid = nn.Sigmoid()
+        self.softplus1 = nn.Softplus()
+        self.bn1 = nn.BatchNorm1d(2 * self.atom_fea_len)
+        self.bn2 = nn.BatchNorm1d(self.atom_fea_len)
+        self.softplus2 = nn.Softplus()
+
+    def forward(self, atom_in_fea, nbr_fea, nbr_fea_idx):
+        """
+        Forward pass
+
+        N: Total number of atoms in the batch
+        M: Max number of neighbors
+
+        Parameters
+        ----------
+
+        atom_in_fea: Variable(torch.Tensor) shape (N, atom_fea_len)
+          Atom hidden features before convolution
+        nbr_fea: Variable(torch.Tensor) shape (N, M, nbr_fea_len)
+          Bond features of each atom's M neighbors
+        nbr_fea_idx: torch.LongTensor shape (N, M)
+          Indices of M neighbors of each atom
+
+        Returns
+        -------
+
+        atom_out_fea: nn.Variable shape (N, atom_fea_len)
+          Atom hidden features after convolution
+
+        """
+        N, M = nbr_fea_idx.shape
+        # convolution
+        atom_nbr_fea = atom_in_fea[nbr_fea_idx, :]
+        total_nbr_fea = torch.cat(
+            [
+                atom_in_fea.unsqueeze(1).expand(N, M, self.atom_fea_len),
+                atom_nbr_fea,
+                nbr_fea,
+            ],
+            dim=2,
+        )
+        total_gated_fea = self.fc_full(total_nbr_fea)
+        total_gated_fea = self.bn1(
+            total_gated_fea.view(-1, self.atom_fea_len * 2)
+        ).view(N, M, self.atom_fea_len * 2)
+        nbr_filter, nbr_core = total_gated_fea.chunk(2, dim=2)
+        nbr_filter = self.sigmoid(nbr_filter)
+        nbr_core = self.softplus1(nbr_core)
+        nbr_sumed = torch.sum(nbr_filter * nbr_core, dim=1)
+        nbr_sumed = self.bn2(nbr_sumed)
+        out = self.softplus2(atom_in_fea + nbr_sumed)
+        return out
+
+
+class ConvLayer_v2(nn.Module):
+    """
+    Convolutional operation on graphs
+    """
+
+    def __init__(self, atom_fea_len, nbr_fea_len):
+        """
+        Initialize ConvLayer.
+
+        Parameters
+        ----------
+
+        atom_fea_len: int
+          Number of atom hidden features.
+        nbr_fea_len: int
+          Number of bond features.
+        """
+        super(ConvLayer_v2, self).__init__()
         self.atom_fea_len = atom_fea_len
         self.nbr_fea_len = nbr_fea_len
         self.fc_full = nn.Linear(
@@ -129,7 +206,7 @@ class CrystalGraphConvNet(nn.Module):
         self.embedding = nn.Linear(orig_atom_fea_len, atom_fea_len)
         self.convs = nn.ModuleList(
             [
-                ConvLayer(atom_fea_len=atom_fea_len, nbr_fea_len=nbr_fea_len)
+                ConvLayer_v1(atom_fea_len=atom_fea_len, nbr_fea_len=nbr_fea_len)
                 for _ in range(n_conv)
             ]
         )
