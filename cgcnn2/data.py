42b2638c66f2aa7c48a61622a9fbcf29ccd14d96
Jiacheng Wang <kashkingwong@gmail.com>
2025-05-28 11:08:02 -0400

    add back cache

--- cgcnn2/data.py
+++ cgcnn2/data.py
@@ -226,11 +226,14 @@ class CIFData(Dataset):
     def __len__(self):
         return len(self.id_prop_data)
 
-    # @functools.lru_cache(maxsize=1024)  # Cache loaded structures
+    @functools.lru_cache(maxsize=1024)  # Cache loaded structures
+    def _load_structure(self, cif_path: str) -> Structure:
+        return Structure.from_file(cif_path)
+
     def __getitem__(self, idx):
         cif_id, target = self.id_prop_data[idx]
-        crystal = Structure.from_file(os.path.join(self.root_dir, cif_id + ".cif"))
-        if self.transform:
+        crystal = self._load_structure(os.path.join(self.root_dir, cif_id + ".cif"))
+        if self.transform is not None:
             crystal = self.transform(crystal)
         atom_fea = np.vstack(
             [
